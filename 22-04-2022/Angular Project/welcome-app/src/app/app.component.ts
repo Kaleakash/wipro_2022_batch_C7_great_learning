9f6fe9deab6265e81a7f2d4f19e7fb502b927243
Akash <akash300383@gmail.com>
2022-04-22 19:33:07 +0530

    done

---
+++ 22-04-2022/Angular Project/welcome-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // <app-root></app-root>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  //title = 'welcome-app';
+  name:string ="Raj Deep"
+}
