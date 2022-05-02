0624e132361b6042e4d6226c53e586c9fef44fe7
Akash <akash300383@gmail.com>
2022-05-02 19:21:49 +0530

    done

--- 29-04-2022/Angular projects/angular-forms/src/app/mdf-login-page/mdf-login-page.component.ts
+++ 29-04-2022/Angular projects/angular-forms/src/app/mdf-login-page/mdf-login-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-mdf-login-page',
   templateUrl: './mdf-login-page.component.html',
@@ -9,8 +8,10 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class MdfLoginPageComponent implements OnInit {
 
   loginRef = new FormGroup({
-    email : new FormControl(),
-    password : new FormControl()
+                        // it is equal to requried attribute in html page 
+    email : new FormControl("",[Validators.required,Validators.minLength(3)]),
+            // ? optional, * 0 to many and + 1 to many 
+    password : new FormControl("",[Validators.required,Validators.pattern("[a-z]+@[0-9]+")])
   })
  msg:string =""
 
@@ -18,7 +19,6 @@ export class MdfLoginPageComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
   checkUser() {
     let login = this.loginRef.value;
     if(login.email=="Raj@gmail.com" && login.password=="123"){
