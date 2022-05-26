41a1f58afc7f6c338919cd914b6624c2430540cf
akash <akash300383@gmail.com>
2022-05-26 19:31:37 +0530

    done

---
+++ 26-05-2022/Spring boot/SpringBootWithRestfull/src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,91 @@
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.attribute.standard.Media;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.Employee;
+
+@RestController
+public class EmployeeController {
+
+	// http://localhost:8080/say 
+	@RequestMapping(value = "say")
+	public String sayHello() {
+		return "Welcome to Spring boot with restful web service";
+	}	
+	// http://localhost:8080/html 
+	@RequestMapping(value = "html",produces = MediaType.TEXT_HTML_VALUE)
+	public String sayHtml() {
+		return "<h2>Welcome to Spring boot with restful web service<h2>";
+	}
+	// http://localhost:8080/xml 
+	@RequestMapping(value = "xml",produces = MediaType.TEXT_XML_VALUE)
+	public String sayXml() {
+		return "<h2>Welcome to Spring boot with restful web service<h2>";
+	}
+	// http://localhost:8080/text
+	@RequestMapping(value = "text",produces = MediaType.TEXT_PLAIN_VALUE)
+	public String sayText() {
+		return "<h2>Welcome to Spring boot with restful web service<h2>";
+	}
+
+	// http://localhost:8080/empInfo
+	@RequestMapping(value = "empInfo",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Employee getEmployee() {
+		Employee emp = new Employee(1, "Ravi", 12000);
+		return emp;
+	}
+	// http://localhost:8080/employees
+	@RequestMapping(value = "employees",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> getAllEmployee() {
+		List<Employee> listOfEmp =new ArrayList<Employee>();
+		listOfEmp.add(new Employee(1, "Ravi", 10000));
+		listOfEmp.add(new Employee(2, "Ramesh", 12000));
+		listOfEmp.add(new Employee(3, "Ajay", 14000));
+		return listOfEmp;
+	}
+	
+	// http://localhost:8080/singleQuery?name=Ravi
+	@RequestMapping(value = "singleQuery")
+	public String singleQueryParam(@RequestParam("name") String name) {
+		return "Welcome to Query param concept "+name;
+	}
+	// http://localhost:8080/multiQuery?name=Ravi&age=21
+	@RequestMapping(value = "multiQuery")
+	public String mutliQueryParam(@RequestParam("name") String name, @RequestParam("age") int age) {
+		return "Welcome to Query param concept "+name+" age is "+age;
+	}
+		// http://localhost:8080/singlePath/Ravi
+		@RequestMapping(value = "singlePath/{name}")
+		public String singlePathParam(@PathVariable("name") String name) {
+			return "Welcome to param param concept "+name;
+		}	
+		// http://localhost:8080/multiPath/Ravi/21
+		@RequestMapping(value = "multiPath/{name}/{age}")
+		public String multiPathParam(@PathVariable("name") String name, @PathVariable("age") int age) {
+			return "Welcome to param param concept "+name+" Age is "+age;
+		}
+		
+		// http://localhost:8080/postMethod
+		@RequestMapping(value = "postMethod")
+		public String testPost() {
+			return "Welcome to Post method";
+		}
+		// http://localhost:8080/storeEmployee 
+		
+		@RequestMapping(value="storeEmployee",consumes = MediaType.APPLICATION_JSON_VALUE)
+		public String storeEmployeeInfo(@RequestBody Employee emp) {
+			System.out.println(emp);
+			return "Welcome user to post method "+emp.getName();
+		}
+		
+}
