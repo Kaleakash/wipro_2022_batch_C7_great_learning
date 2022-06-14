2f1c6ca176518b72d4a7575c87797b4f34a0863b
akash <akash300383@gmail.com>
2022-06-14 19:31:32 +0530

    done

---
+++ 14-06-2022/Creating image to run Spring boot application/SpringBootWithDocker/SpringBootWithDocker/src/main/java/com/controller/SimpleController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("docker")
+public class SimpleController {
+
+	@GetMapping(value = "sayHello")
+	public String sayHello() {
+		return "Welcome to Spring boot with Docker ";
+	}
+}
---
+++ 14-06-2022/Image to run the Java Application/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String args[]){
+        System.out.println("Welcome to Simple Java Programming running through Docker");
+    }
+}
